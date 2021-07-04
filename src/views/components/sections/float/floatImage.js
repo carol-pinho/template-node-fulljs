0be0707e0a8184f8d62122eac66970a2ddf8875c
Anne Caroline Pinho Valentim <anne.pinhov@gmail.com>
2021-07-04 14:22:07 -0400

    primeiro commit

---
+++ src/views/components/sections/float/floatImage.js
@@ -0,0 +1,12 @@
+import YodaBaby from '../../../../assets/imagens/elements/baby-yoda.png'
+
+
+
+let floatImage = `
+<div class="main-left">
+    <img src="${YodaBaby}" alt="Yoda Baby" width="120px" height="auto">
+</div>
+
+`
+
+export default floatImage;
---
+++ src/views/components/sections/homeSectionOne.js
@@ -0,0 +1,11 @@
+import Logo from '../../../assets/imagens/logo/logo.png';
+import backgroundOne from '../../../assets/imagens/background/bg1.jpg';
+
+let HomeSectionOne = `
+<section class="section background--one" style="background-image: url(${backgroundOne})">
+    <h1>CLIQUE NO BABY YODA, ELE TE MOSTRARÁ O CAMINHO</h1>
+    <img src=${Logo} alt="Logo Star Wars" width= "300px" height="auto">
+</section>
+`
+
+export default HomeSectionOne
